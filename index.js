f6935289e0d274226acd9593c3d0d0d1565b0c17
Jesus Ramirez main laptop <jesus.ramirez.bocanegra@gmail.com>
2021-11-28 01:38:46 -0600

    Finished with Create and Request, still missing Update and Destroy

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const db = firebase.firestore();
-const taskForm = document.getElementById('task-form');
+const productForm = document.getElementById('product-form');
+const productContainer = document.getElementById('products-container');
+const saveProduct = (name, cost, price) => db.collection('products').doc().set({
+    name,
+    cost,
+    price
+});
 
-taskForm.addEventListener('submit', async(e) => {
-    e.preventDefault();
-    const name = taskForm['product-name'].value;
-    const price = taskForm['product-price'].value;
-    const response = await db.collection('products').doc().set({
-        name,
-        price
+const getProduct = () => db.collection('products').get();
+
+const onGetProduct = (callback) => db.collection('products').onSnapshot(callback);
+
+window.addEventListener('DOMContentLoaded', async(e) => {
+    onGetProduct((querySnapshot) => {
+        productContainer.innerHTML = '';
+        querySnapshot.forEach(doc => {
+            console.log(doc.data());
+            const product = doc.data();
+            productContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
+                <h3 class="h5">${product.name}</h3>
+                <p>${product.price}</p>
+                <div>
+                    <button class="btn btn-primary">Delete</button>
+                    <button class="btn btn-secondary">Edit</button>
+                </div>
+            </div>`
+        });
     })
-    console.log(response);
-    console.log(title, description);
+
+});
+
+productForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const name = productForm['product-name'];
+    const cost = productForm['product-cost'];
+    const category = productForm['product-category'];
+    const price = (Number(cost.value)) * (Number(category.value));
+
+    await saveProduct(name.value, cost.value, price);
+
+    await getProduct();
+
+    productForm.reset();
+    name.focus
 });
